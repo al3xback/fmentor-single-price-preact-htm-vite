52dc869e6a530e3b93849b5c0ea5a08c62a6ce7a
alex <al3xback@gmail.com>
2024-06-11 21:09:40 +0700

    changed className to class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,6 @@ import { html } from 'htm/preact';
 
 export default function Header() {
 	return html`<header>
-		<h1 className="sr-only">Single price grid component</h1>
+		<h1 class="sr-only">Single price grid component</h1>
 	</header>`;
 }
